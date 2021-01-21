4e8f253de78e8711ea7b8d9ce364f0909e8b24ce
Isac Campos <isacsouzacampos@hotmail.com>
2021-01-21 00:21:09 -0300

    Adicionada Splash Screen.

--- app/src/main/java/com/example/apppietra/MainActivity.java
+++ app/src/main/java/com/example/apppietra/MainActivity.java
@@ -3,31 +3,12 @@ package com.example.apppietra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar progressBar;
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        progressBar = findViewById(R.id.progress_bar);
-        textView = findViewById(R.id.percent);
-
-        progressBar.setMax(100);
-        progressBar.setScaleY(3f);
-
-        progressAnimation();
-    }
-
-    public void progressAnimation() {
-        ProgressBarAnimation anim = new ProgressBarAnimation(this, progressBar, textView,
-                0f, 100f);
-        anim.setDuration(8000);
-        progressBar.setAnimation(anim);
     }
 }
