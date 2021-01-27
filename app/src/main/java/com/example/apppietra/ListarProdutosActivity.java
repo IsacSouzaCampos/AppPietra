bfcca65b65c3d16fe07d5e8c30c1cc9b62123615
Isac Campos <isacsouzacampos@hotmail.com>
2021-01-27 14:13:03 -0300

    Criado menu principal com ícones de pesquisar e inserir. Apenas o botão de inserir está funcional.

--- app/src/main/java/com/example/apppietra/ListarProdutosActivity.java
+++ app/src/main/java/com/example/apppietra/ListarProdutosActivity.java
@@ -2,7 +2,11 @@ package com.example.apppietra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,7 +17,7 @@ public class ListarProdutosActivity extends AppCompatActivity {
     private ListView listViewProdutos;
     private ProdutoDataAccessObject dao;
     private List<Produto> produtos;
-    private List<Produto> produtosFiltrados = new ArrayList<Produto>();
+    private List<Produto> produtosFiltrados = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +29,27 @@ public class ListarProdutosActivity extends AppCompatActivity {
         produtos = dao.obterProdutos();
         produtosFiltrados.addAll(produtos);
         ArrayAdapter<Produto> adaptador = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, produtos);
+                android.R.layout.simple_list_item_1, produtosFiltrados);
         listViewProdutos.setAdapter(adaptador);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    public void cadastrar(MenuItem item) {
+        Intent it = new Intent(this, InserirActivity.class);
+        startActivity(it);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        produtos = dao.obterProdutos();
+        produtosFiltrados.clear();
+        produtosFiltrados.addAll(produtos);
+        listViewProdutos.invalidateViews();
     }
 }
